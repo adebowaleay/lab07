d215dbd3d5c63a783d212d85c3d48ad9923e2e38
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-21 01:17:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 function init(){
 //add your javascrip between these two lines of code
+
+ synt
+ document.addEventListener('touchstart', function(event) {
+    alert(event.touches.length);
+}, false);
+ /*if/else*/
  
+document.getElementById("div").addEventListener('touchstart', function(){
+	alert('Ayobami Adebowale has seen your message: ' + document.getElementById(input).xx);
+	document.getElementById('textoutput').innerHTML = document.getElementById(input).xx;
+})(event.touches.length;false);
 
-
-
-
-
+}
 
 
 window.addEventListener('load', init);
