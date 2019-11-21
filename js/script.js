651c3df7526c03b528fa7109889e994b0688fecd
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-21 02:37:36 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 function init(){
-//add your javascrip between these two lines of code
 
 document.getElementById('entrybutton').addEventListener('click', function(){alert('Ayobami Adebowale has seen your message: ' + document.getElementById('entryinput').value);
 	document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;
 });}
-
+/*if syntax?*/
 window.addEventListener('load', init);
