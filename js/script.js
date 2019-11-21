6f977cb21445644e42f5a8652eeea63beea23cb5
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2019-11-21 01:27:35 -0600

    Update script.js
    fixed syntax

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
 function init(){
 //add your javascrip between these two lines of code
 
- synt
- document.addEventListener('touchstart', function(event) {
-    alert(event.touches.length);
-}, false);
- /*if/else*/
- 
-document.getElementById("div").addEventListener('touchstart', function(){
-	alert('Ayobami Adebowale has seen your message: ' + document.getElementById(input).xx);
-	document.getElementById('textoutput').innerHTML = document.getElementById(input).xx;
-})(event.touches.length;false);
-
-}
-
+document.getElementById('entrybutton').addEventListener('click', function(){alert('Ayobami Adebowale has seen your message: ' + document.getElementById('entryinput').value);
+	document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;
+});}
 
 window.addEventListener('load', init);
